5c6409e33978d566cb421d84eb9d9c2f4c52dc82
Priyansh Gupta <priyansh.dev02@gmail.com>
2024-06-13 02:06:10 +0530

    Created the UI of intro Screen

--- lib/Screens/IntroScreen.dart
+++ lib/Screens/IntroScreen.dart
@@ -11,7 +11,37 @@ class _IntroScreenState extends State<IntroScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: MediaQuery.sizeOf(context).height * 0.8,
+              width: MediaQuery.sizeOf(context).width * 0.6,
+              child: Image.asset("assets/images/Logo.png"),
+            ),
+            Container(
+              height: 50,
+              width: MediaQuery.sizeOf(context).width*0.8,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xff042628),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  )
+                ),
+                  onPressed: () {},
+                  child: Text(
+                    'Get Started',
+                    style: TextStyle(
+                        fontFamily: 'SofiaPro',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  )),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mealtime/Screens/IntroScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Meal Time',
       debugShowCheckedModeBanner: false,
+      home: IntroScreen(),
       );
   }
 }
